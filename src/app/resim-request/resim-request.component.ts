b01622922508061ddea52f979adbbe9a2b654a3c
EENSCHOOTEN Alain (AMPERE) <alain.eenschooten@ampere.cars>
2025-03-18 16:40:39 +0100

    Agencing elements resim-request component

--- src/app/resim-request/resim-request.component.ts
+++ src/app/resim-request/resim-request.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-resim-request',
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule, ReactiveFormsModule],
   templateUrl: './resim-request.component.html',
   styleUrl: './resim-request.component.css'
 })
 export class ResimRequestComponent {
+  faCaretDown = faCaretDown;
+  faCaretUp = faCaretUp;
+  
+  formGroup = new FormGroup({
+    resim_comments : new FormControl(''),
+    results_comments : new FormControl(''), 
+  });
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+  }
 
 }
